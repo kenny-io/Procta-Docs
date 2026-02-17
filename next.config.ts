13f3f292b17b28610c444d2081e03131d7b1b598
kenny-io <ekeneeze3@gmail.com>
2026-02-17 04:44:47 +0400

    use turbopack for builds

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,10 @@
-import createMDX from '@next/mdx'
 import type { NextConfig } from 'next'
 
-import { rehypePlugins } from './src/mdx/rehype'
-import { remarkPlugins } from './src/mdx/remark'
-
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins,
-    rehypePlugins,
-  },
-})
-
 const nextConfig: NextConfig = {
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ['ts', 'tsx'],
   experimental: {
     externalDir: true,
   },
 }
 
-export default withMDX(nextConfig)
+export default nextConfig
